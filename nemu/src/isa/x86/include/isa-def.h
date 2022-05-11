cd53c0b96ea0de5b01fe8fa2c4ad2e0e31f12605
cuikangyuan3 <cuikangyuan@jd.com>
2022-05-11 22:57:08 +0800

    add x86 cpu struct

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -3,10 +3,40 @@
 
 #include <common.h>
 
-typedef struct {
-  
+typedef struct
+{
+
+  struct
+  {
+    union
+    {
+      union
+      {
+        uint32_t _32;
+        uint16_t _16;
+        uint8_t _8[2];
+      } gpr[8];
+
+      struct
+      {
+        /* Do NOT change the order of the GPRs' definitions. */
+
+        /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+         * in PA2 able to directly access these registers.
+         */
+        rtlreg_t eax;
+        rtlreg_t ecx;
+        rtlreg_t edx;
+        rtlreg_t ebx;
+        rtlreg_t esp;
+        rtlreg_t ebp;
+        rtlreg_t esi;
+        rtlreg_t edi;
+      };
+    };
+  };
+
   vaddr_t pc;
 } x86_CPU_state;
-
 
 #endif
