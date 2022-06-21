2bc6099288a72b6826c08472e34ea83bb2959017
cuikangyuan <cuikangyuan@outlook.com>
2022-06-21 20:25:53 +0800

    finish pa2.1

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,7 +1,8 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(addi) f(jal) f(auipc) f(nemu_trap)
-
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(addi) f(jal) f(auipc) f(jalr) f(nemu_trap)
+//t0, t1, ... - 只能在RTL伪指令的实现过程中存放中间结果
+//s0, s1, ... - 只能在译码辅助函数和执行辅助函数的实现过程中存放中间结果
 //每个执行辅助函数都需要有一个标识该指令的ID以及一个表格辅助函数与之相对应, 这一点是通过一系列宏定义来实现的
 def_all_EXEC_ID();
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -14,3 +14,11 @@ def_EHelper(jal) {
   rtl_addi(s, ddest, &s->pc, 4);
   rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
 }
+
+
+def_EHelper(jalr) {
+  rtl_addi(s, s0, &s->pc, 4);
+  rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
+  rtl_andi(s, &s->dnpc, &s->dnpc, ~1);
+  rtl_addi(s, ddest, s0, 0);
+}
